4bf547223eda2228f4747cafbd88f9d50494bbe8
Victor Jian Choe <vchoe89@gmail.com>
2019-04-14 19:19:36 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,5 +1,6 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
 def display_board
+<<<<<<< HEAD
   puts "   |   |   "
   puts "-----------"
   puts "   |   |   "
@@ -7,4 +8,6 @@ def display_board
   puts "   |   |   "
 
 end
+=======
+>>>>>>> aae82bc2318e52afed84d09bbba66c117ac89ca1
   
